6e65d8e84ca64b7f590eb9a6ba3ee2ed730bb937
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-08-06 16:06:24 -0400

    Keep sampleTrack private to Sample constructor.

--- Sample.js
+++ Sample.js
@@ -1,12 +1,11 @@
 ;(function(exports) {
   var Sample = function(fname, sampleTrack) {
-	  this.sampleTrack = sampleTrack;
 	  this.fname = fname;
 	  this.audio = $("<audio controls><source src='" + this.fname + "' type ='audio/mpeg'></audio>");
 	  this.domElement = new dom.SampleDom(this);
 	  // this.domElement.draw();
 	  this.play = function () {
-		  if (this.sampleTrack.on) {
+		  if (sampleTrack.on) {
 			  this.audio.get(0).play();
 			  this.audio = $("<audio controls><source src='" + this.fname + "' type ='audio/mpeg'></audio>");
 		  }
